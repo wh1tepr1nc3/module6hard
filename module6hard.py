bc29b94822193d185a7724d475dfbbd2031f3286
wh1tepr1nc3 <amamamania@mail.ru>
2024-10-13 13:00:01 +0500

    Add files via upload

---
+++ module6hard.py
@@ -0,0 +1,88 @@
+class Figure:
+
+
+    def __init__(self, color, *sides):
+        self.__sides = list(sides)
+        self.__color = list(color)
+        self.filled = False
+
+    @property
+    def sides_count(self):
+        return 0
+
+    @staticmethod
+    def __is_valid_color(r, g, b):
+        return all(0 <= x <= 255 for x in (r, g, b))
+
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
+
+    def get_color(self):
+        return self.__color
+
+    def __is_valid_sides(self, *new_sides):
+        return len(new_sides) == self.sides_count and all(isinstance(x, int) and x > 0 for x in new_sides)
+
+    def set_sides(self, *new_sides):
+        if self.__is_valid_sides(*new_sides):
+            self.__sides = list(new_sides)
+
+    def get_sides(self):
+        return self.__sides
+
+    def __len__(self):
+        return sum(self.__sides)
+
+
+class Circle(Figure):
+    sides_count = 1
+
+    def __init__(self, color, length):
+        super().__init__(color, length)
+        self.__radius = length / (2 * 3.14)
+
+    def get_square(self):
+        return 3.14 * (self.__radius ** 2)
+
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def get_square(self):
+        s = sum(self.get_sides()) / 2
+        return (s * (s - self.get_sides()[0]) * (s - self.get_sides()[1]) * (s - self.get_sides()[2])) ** 0.5
+
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, color, edge):
+        sides = [edge] * self.sides_count
+        super().__init__(color, *sides)
+
+    def get_volume(self):
+        edge = self.get_sides()[0]
+        return edge ** 3
+
+
+circle1 = Circle((200, 200, 100), 10)
+cube1 = Cube((222, 35, 130), 6)
+
+
+circle1.set_color(55, 66, 77)
+print(circle1.get_color())
+cube1.set_color(300, 70, 15)
+print(cube1.get_color())
+
+
+cube1.set_sides(5, 3, 12, 4, 5)
+print(cube1.get_sides())
+circle1.set_sides(15)
+print(circle1.get_sides())
+
+
+print(len(circle1))
+
+
+print(cube1.get_volume())
